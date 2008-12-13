d7df11b4def5228aa5e35b72901dff766e2514ff
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-13 21:13:17 +0000

    Added chatterl_serv a gen_server used for the chat server

---
+++ chatterl_serv.erl
@@ -0,0 +1,126 @@
+%%%-------------------------------------------------------------------
+%%% File    : chatterl_serv.erl
+%%% Author  : Yomi Akindayini <yomi@boodah.net>
+%%% Description : Chatterl server, used to manage users.
+%%%
+%%% Created : 13 Dec 2008 by Yomi Akindayini <yomi@boodah.net>
+%%%-------------------------------------------------------------------
+-module(chatterl_serv).
+
+-behaviour(gen_server).
+
+%% API
+-export([start/0,stop/0,add_session/2,remove_session/1]).
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(chatterl, {sessions}).
+-record(state, {}).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop() ->
+    gen_server:call(?MODULE, stop).
+
+add_session(Login, Password) ->
+    gen_server:call({global, ?MODULE}, {add_session, Login, Password}, infinity).
+
+remove_session(Login) ->
+    gen_server:call({global, ?MODULE}, {remove_session, Login}, infinity).
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(stop, _Client, State) ->
+    {stop, normal, stopped, State};
+
+handle_call({add_session, Login, Password}, _From, State) ->
+    NewTree =  case gb_trees:is_defined(Login, State#chatterl.sessions) of
+        true ->
+		       Result = "Already have a session",
+		       State#chatterl.sessions;
+        false -> 
+		       Result = "Created session",
+		       gb_trees:insert(Login, {Login, Password}, State#chatterl.sessions)
+    end,
+    {reply, Result, State#chatterl{ sessions = NewTree }};
+
+handle_call({remove_session, Login}, _From, State) ->
+    NewTree =  case gb_trees:is_defined(Login, State#chatterl.sessions) of
+        true -> 
+		       Result = "Session dropped",
+		       gb_trees:delete(Login, State#chatterl.sessions);
+        false -> 
+		       Result = "Unable to drop session.",
+		       State#chatterl.sessions
+    end,
+    {reply, Result, State#chatterl{ sessions = NewTree }}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
