eb0883784668974574964d521575458af19005d3
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-13 18:09:59 +0000

    Added doc header, will work on functionality in a while.

--- chatterl_client.erl
+++ chatterl_client.erl
@@ -1,3 +1,10 @@
+%%%-------------------------------------------------------------------
+%%% File    : chatterl_client.erl
+%%% Author  : Yomi Akindayini <yomi@boodah.net>
+%%% Description : Erlang based chat client
+%%%
+%%% Created : 13 Dec 2008 by Yomi Akindayini <yomi@boodah.net>
+%%%-------------------------------------------------------------------
 -module(chatterl_client).
 
 -export([start/0]).
