fcf07f6a3e88aa2531843d369b036dcefaed10b1
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-14 18:08:01 +0000

    Refactored so we retrieved our results in the correct format.

--- chatterl_groups.erl
+++ chatterl_groups.erl
@@ -10,10 +10,10 @@
 -define(SERVER, chatterl_groups).
 -define(CHATTERL, chatterl_serv).
 
--export([start/0,stop/0,connect/1,loop/1]).
+-export([start/0,stop/0,connect/1,handle_group/1]).
 
 start() ->
-    Pid = spawn(chatterl_groups, loop, [dict:new()]),
+    Pid = spawn(chatterl_groups, handle_group, [dict:new()]),
     case chatterl_serv:start() of
 	{ok, ServPid} ->
 	    io:format("Starting chatterl_serv~n");
@@ -28,11 +28,16 @@ stop() ->
 connect(Group) ->
     ?SERVER ! {connect, Group}.
 
-loop(Users) ->
+handle_group(Users) ->
     receive
 	{connect, Group} ->
-	    chatterl_serv:create(Group, ?SERVER),
-	    loop(Users);
+	    case chatterl_serv:create(Group, ?SERVER) of
+		{ok, Msg} ->
+		    io:format("Created group: ~p~n", [Msg]);
+		{error, Error} ->
+		    io:format("Error: ~p~n", [Error])
+	    end,
+	    handle_group(Users);
 	shutdown ->
 	    io:format("Shutting down...~n")
     end.
--- chatterl_serv.erl
+++ chatterl_serv.erl
@@ -84,10 +84,10 @@ handle_call(view_groups, _Client, State) ->
 handle_call({create, GroupPid, Name}, _From, State) ->
     NewTree =  case gb_trees:is_defined(GroupPid, State#chatterl.groups) of
         true ->
-		       Result = "Group already created",
+		       Result = {ok,"Group already created"},
 		       State#chatterl.groups;
         false -> 
-		       Result = "Created group.",
+		       Result = {error,"Created group."},
 		       gb_trees:insert(GroupPid, {GroupPid, Name}, State#chatterl.groups)
     end,
     {reply, Result, State#chatterl{ groups = NewTree }};
