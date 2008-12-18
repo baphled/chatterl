49dc387c7f24956cf9d33d03f5e515a0aca875f2
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-18 18:42:45 +0000

    Added commenting, will start refactoring gen_servers to use these records and prepare for better data management (mnesia or ets).

--- lib/chatterl/include/chatterl.hrl
+++ lib/chatterl/include/chatterl.hrl
@@ -8,5 +8,7 @@
 
 %% Records for chatterl_serv, will track the total users & groups.
 -record(chatterl, {groups, users}).
+%% Records associated with groups
 -record(groups, {name, description, users}).
+%% Records associated with users
 -record(users, {name, ip}).
