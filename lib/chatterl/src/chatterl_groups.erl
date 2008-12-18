2ce87c23ceb08c770877bad710d6917dfb2068d8
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-18 13:37:18 +0000

    Added print message for when the server terminates.

--- lib/chatterl/src/chatterl_groups.erl
+++ lib/chatterl/src/chatterl_groups.erl
@@ -149,6 +149,7 @@ handle_info(_Info, State) ->
 %% The return value is ignored.
 %%--------------------------------------------------------------------
 terminate(_Reason, _State) ->
+    io:format("Shutting down Chatterl Groups...~n"),
     ok.
 
 %%--------------------------------------------------------------------
