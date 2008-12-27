67530e0d2fa02f512a65c76819694e91177813ea
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-27 10:27:12 +0000

    Refactored client so that when we init the gen_server we login to chatterl_serv any issues will cause the gen_server to shutdown.

--- lib/chatterl/src/chatterl_client.erl
+++ lib/chatterl/src/chatterl_client.erl
@@ -1,13 +1,32 @@
 %%%-------------------------------------------------------------------
 %%% File    : chatterl_client.erl
-%%% Author  : Yomi Akindayini <yomi@boodah.net>
-%%% Description : Erlang based chat client
+%%% Author  :  <>
+%%% Description : 
 %%%
-%%% Created : 13 Dec 2008 by Yomi Akindayini <yomi@boodah.net>
+%%% Created : 27 Dec 2008 by  <>
 %%%-------------------------------------------------------------------
 -module(chatterl_client).
 
--export([login/1]).
+-behaviour(gen_server).
+
+%% API
+-export([start/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-record(user, {name, ip}).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start(User) ->
+    gen_server:start_link({global, User}, ?MODULE, [User], []).
 
 login(User) ->
     case chatterl_serv:connect(User) of
@@ -16,3 +35,77 @@ login(User) ->
 	{ok, Message} ->
 	    io:format("~p is ~p.~n", [User, Message])
     end.
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([User]) ->
+    case gen_server:call({global, chatterl_serv}, {connect, User}, infinity) of
+	{error, Error} ->
+	    io:format("~p~n", [Error]),
+	    {stop, Error};
+	{ok, Message} ->
+	    io:format("~p is ~p.~n", [User, Message]),
+	    {ok, #user{name = User}}
+    end.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(get_name, _From, State) ->
+    Reply = State#user.name,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
+
+
