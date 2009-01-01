a9d637f57718dbe9831057f12372b7a047d8a176
Yomi (baphled boodah) Colledge <baphled@boodah.net>
2009-01-01 01:55:46 +0000

    Correct error with edocs

--- lib/chatterl/src/chatterl_groups.erl
+++ lib/chatterl/src/chatterl_groups.erl
@@ -55,7 +55,7 @@ stop() ->
 %% @doc
 %% Initialises our group process.
 %%
-%% @spec init([Name,Description]) -> {ok, State} |
+%% @spec init(Args) -> {ok, State} |
 %%                         {ok, State, Timeout} |
 %%                         ignore               |
 %%                         {stop, Reason}
