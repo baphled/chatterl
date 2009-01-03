107bbf96bb0c02c996b5349e8776d2283c9eede3
Yomi (baphled boodah) Colledge <baphled@boodah.net>
2009-01-03 22:12:08 +0000

    Added to support the new layout of a parent supervisor handling a number of children supervisors.

---
+++ lib/chatterl/src/server_sup.erl
@@ -0,0 +1,70 @@
+%%%----------------------------------------------------------------
+%%% @author  Yomi Akindayini <yomi@boodah.net>
+%%% @doc Supervisor for the Chatterl server, is linked to the
+%%% main Chatterl supervisor.
+%%% @end
+%%% @copyright 2008 Yomi Akindayini
+%%%----------------------------------------------------------------
+-module(server_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    RestartStrategy = one_for_all,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 3600,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000,
+    Type = worker,
+
+    Server = {'Server', {chatterl_serv, start, []},
+              Restart, Shutdown, Type, [chatterl_serv]},
+    {ok, {SupFlags, [Server]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+
