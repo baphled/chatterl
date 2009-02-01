292ee92715234c82ea333fef16716285cd37e321
baphled <baphled@makrhoe.local>
2009-02-01 20:45:58 +0000

    Refactored to take advantage of chatterl_mid_mans JSON & XML response functionality, as mentioned this functionality will be placed in another module, though the functionality here will still remain the same.

--- lib/chatterl/src/chatterl_gateway.erl
+++ lib/chatterl/src/chatterl_gateway.erl
@@ -8,7 +8,7 @@
 %%% Allows Chatterl to interface with any web-based interface
 %%% Using JSON and XML, sending the requests off to the chatterl_serv
 %%% module.
-%%% 
+%%%
 %%% All calls to CWIGA will only be allowed via a specified IP, which
 %%% will be defined with the configuration file.
 %%% @end
@@ -52,9 +52,9 @@ start(Port) ->
 %% @end
 %%--------------------------------------------------------------------
 dispatch_requests(Req) ->
-    [Path|Ext] = string:tokens(Req:get(path),"."),
-    Action = clean_path(Path),
-    handle(Action,Ext,Req).
+  [Path|Ext] = string:tokens(Req:get(path),"."),
+  Action = clean_path(Path),
+  handle(Action,get_content_type(Ext),Req).
 
 %%====================================================================
 %% gen_server callbacks
@@ -91,7 +91,7 @@ init([Port]) ->
 %%--------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
     {reply,ok,State}.
-    
+
 %%--------------------------------------------------------------------
 %% @doc
 %% Handling cast messages
@@ -177,10 +177,10 @@ clean_path(Path) ->
 get_content_type(Type) ->
     case Type of
 	["json"] ->
-	    "text/plain";
+	    ["text/json"];
 	["xml"] ->
-	    "text/xml";
-	_ -> "text/plain"
+	    ["text/xml"];
+	_ -> ["text/json"]
     end.
 
 %%--------------------------------------------------------------------
@@ -196,41 +196,41 @@ handle(Path,ContentType,Req) ->
     Response =
 	case Path of
 	    %% Client based requests
-	    "/connect/" ++ Client -> chatterl_mid_man:connect(Client);
-	    "/disconnect/" ++ Client -> chatterl_mid_man:disconnect(Client);
-	    "/users/list" -> chatterl_mid_man:user_list();
-	    "/users/list/" ++Group -> chatterl_mid_man:user_list(Group);
+	    "/connect/" ++ Client -> chatterl_mid_man:connect(ContentType,Client);
+	    "/disconnect/" ++ Client -> chatterl_mid_man:disconnect(ContentType,Client);
+	    "/users/list" -> chatterl_mid_man:user_list(ContentType);
+	    "/users/list/" ++Group -> chatterl_mid_man:user_list(ContentType,Group);
 	    "/users/send/" ++ Sender -> [Client, Message] = get_properties(Req,["client","msg"]),
-					chatterl_mid_man:user_msg(Sender,Client,Message);
-	    "/users/poll/" ++ Client -> chatterl_mid_man:user_poll(Client);
+					chatterl_mid_man:user_msg(ContentType,{Sender,Client,Message});
+	    "/users/poll/" ++ Client -> chatterl_mid_man:user_poll(ContentType,Client);
 	    %% Group based requests
-	    "/groups/list" -> chatterl_mid_man:group_list();
-	    "/groups/info/" ++ Group -> chatterl_mid_man:group_info(Group);
+	    "/groups/list" -> chatterl_mid_man:group_list(ContentType);
+	    "/groups/info/" ++ Group -> chatterl_mid_man:group_info(ContentType,Group);
 	    "/groups/join/" ++ Group -> [Client] = get_properties(Req,["client"]),
-					chatterl_mid_man:group_join(Group,Client);
+					chatterl_mid_man:group_join(ContentType,{Group,Client});
 	    "/groups/leave/" ++ Group -> [Client] = get_properties(Req,["client"]),
-					 chatterl_mid_man:group_leave(Group,Client);
+					 chatterl_mid_man:group_leave(ContentType,{Group,Client});
 	    "/groups/send/" ++ Group -> [Sender, Message] = get_properties(Req,["client","msg"]),
-					chatterl_mid_man:group_send(Group,Sender,Message);
-	    "/groups/poll/" ++ Group -> chatterl_mid_man:group_poll(Group);
+					chatterl_mid_man:group_send(ContentType,{Group,Sender,Message});
+	    "/groups/poll/" ++ Group -> chatterl_mid_man:group_poll(ContentType,Group);
 	    %% Authentication based queries.
-	    "/groups/create/" ++ Group -> 
+	    "/groups/create/" ++ Group ->
 		[Description] = get_properties(Req,["description"]),
 		case is_auth(Req) of
-		    {ok, _Msg} -> 
-			chatterl_mid_man:group_create(Group,Description);
+		    {ok, _Msg} ->
+			chatterl_mid_man:group_create(ContentType,{Group,Description});
 		    {error,Error} -> chatterl_mid_man:build_carrier("error",Error)
 		end;
 	    "/groups/drop/" ++ Group -> case is_auth(Req) of
-					    {ok,_Msg} -> 
-						chatterl_mid_man:group_drop(Group);
-					    {error,Error} -> 
+					    {ok,_Msg} ->
+						chatterl_mid_man:group_drop(ContentType,Group);
+					    {error,Error} ->
 						chatterl_mid_man:build_carrier("error",Error)
 					end;
 	    %% Catch all
 	    Unknown -> chatterl_mid_man:build_carrier("error", "Unknown command: " ++Unknown)
     end,
-    send_response(Req,{get_content_type(ContentType),Response}).
+  Req:respond({200, [{"Content-Type", ContentType}], list_to_binary(Response)}).
 
 %%--------------------------------------------------------------------
 %% @private
@@ -249,44 +249,7 @@ get_properties(Req,WantedParams) ->
 %% @private
 %% @doc
 %%
-%% Handles our all responses.
-%%
-%% Sends responses based on the content type, which are JSON and XML.
-%% @spec send_response(Req,{ContentType,Record}) -> void()
-%%
-%% @end
-%%--------------------------------------------------------------------
-send_response(Req, {ContentType,Record}) when is_list(ContentType) ->
-    Response = get_response_body(ContentType,Record),
-    Code = get_response_code(Record),
-    Req:respond({Code, [{"Content-Type", ContentType}], list_to_binary(Response)}).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Gets the actual response body to return to the client.
-%%
-%% Responses are either in JSON or XML, if the wrong content type is passed
-%% the client will recieve an error in XML format.
-%% @spec get_response_body(ContentType,Record) -> [ResponseBody]
-%%
-%% @end
-%%--------------------------------------------------------------------
-get_response_body(ContentType,Record) ->
-    case ContentType of
-	"text/plain" ->
-	    json_message(Record);
-	"text/xml" ->
-	    xml_message(Record);
-	_ -> json_message(chatterl_mid_man:build_carrier("error","Illegal content type!"))
-    end.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Gets our response code depending on the type of message passed by 
+%% Gets our response code depending on the type of message passed by
 %% the carrier record.
 %%
 %% @spec get_response_code(Record) -> integer()
@@ -303,276 +266,6 @@ get_response_code(Record) ->
 		_ -> 500
 	    end
     end.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Converts our carrier Record into a JSON format.
-%% @spec json_message(CarrierRecord) -> JSON
-%%
-%% @end
-%%--------------------------------------------------------------------
-json_message(CarrierRecord) ->
-    {carrier, CarrierType, Message} = CarrierRecord, 
-    Struct =
-	case Message of
-	    {carrier,Type,MessagesCarrier} ->
-		case Type =:= "groups" 
-		    orelse Type =:= "clients" 
-		    orelse Type =:= "messages" of
-		    true ->
-			handle_messages_json(Type,MessagesCarrier,CarrierType);
-		    false ->
-			io:format("dont know ~s~n",[Type])
-		end;
-	    _ ->
-		{struct,[{CarrierType,list_to_binary(Message)}]}
-	end,
-    mochijson2:encode({struct,[{chatterl,{struct,[{response,Struct}]}}]}).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Generates out actual XML message.
-%%
-%% Takes the record and converts it into a tuple which can be further
-%% converted into a valid XML format using tuple_to_xml.
-%% Example of XML tuple structure.
-%% <code>{carrier,"groups",[{carrier,"group","nu"},{carrier,"group","nu2"}]}</code>
-%%
-%% @spec xml_message(Record) -> XML
-%%
-%% @end
-%%--------------------------------------------------------------------
-xml_message(CarrierRecord) ->
-    {carrier, MessageType, Message} = CarrierRecord,
-    XMLTuple = case Message of
-	{carrier, Type, Record} ->		
-	    case Type =:= "groups" 
-		orelse Type =:= "clients" 
-		orelse Type =:= "messages" of
-		true -> 
-		    handle_messages_xml(Type,Record,MessageType);
-		false -> io:format("dont know ~s~n",[Type])
-	    end;
-	_ -> xml_tuple_single(MessageType,Message)
-    end,
-    tuple_to_xml(XMLTuple,[]).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Generates JSON structure needed to create message responses.
-%%
-%% As messages are a different format that the other responses, they
-%% need to be handled uniquely. We also want to make sure that all three
-%% clauses are meet (empty,single message, multiple messages).
-%% @spec handle_messages_json(CarrierType,MessagesCarrier,Type) -> JSON
-%%
-%% @end
-%%--------------------------------------------------------------------
-handle_messages_json(Type,MessagesCarrier,CarrierType) ->
-    case Type =:= "messages" of
-	true ->
-	    case MessagesCarrier of
-		[] -> %Empty list.
-		    {struct,[{Type,[]}]};
-		[{carrier,_MessageType,MessageData}] ->	% A Single message.
-		    {struct,[{CarrierType,
-			      {struct,[{Type,loop_json_carrier(MessageData)}]}}]};
-		Messages -> % Multiple messages
-		    {struct,[{CarrierType,
-			      {struct,[{Type,inner_loop_json_carrier(Messages)}]}}]}
-	    end;
-	false ->
-	    Result = loop_json_carrier(MessagesCarrier),
-	    {struct,[{CarrierType,{struct,[{Type,Result}]}}]}
-    end.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Generates XML structure needed to create message responses.
-%%
-%% @spec handle_messages_xml(Type,CarrierType,MessagesCarrier) -> XMLTuple
-%%
-%% @end
-%%--------------------------------------------------------------------
-handle_messages_xml(Type,MessagesCarrier,CarrierType) ->
-    case Type =:= "messages" of
-	true ->
-	    case MessagesCarrier of
-		_ -> 
-		    Result = inner_loop_xml_tuple(MessagesCarrier),
-		    Data = loop_xml_tuple(Type,Result),
-		    xml_tuple_single(CarrierType,Data)
-	    end;
-	false ->
-	    Result = loop_xml_carrier(MessagesCarrier),
-	    xml_tuple(Type,[Result])
-    end.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Loops over the record carrier building the tuple structure need to
-%% build our JSON.
-%% @spec loop_json_carrier(Carrier) -> JSONTuple
-%%
-%% @end
-%%--------------------------------------------------------------------
-loop_json_carrier(CarrierRecord) ->
-    [{struct,[{list_to_binary(DataType),clean_message(Data)}]} || {carrier,DataType,Data} <- CarrierRecord].
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Loops over the message carrier building the tuple structure need to
-%% build our JSON.
-%%
-%% We use this to build each message retrieved from a group, as they
-%% have a number of elements we need to parse those the same as the
-%% outer elements.
-%% @spec inner_loop_json_carrier(CarrierRecord) -> JSONTuple
-%%
-%% @end
-%%--------------------------------------------------------------------
-inner_loop_json_carrier(CarrierRecord) ->
-    [{struct,[{list_to_binary(MsgType),loop_json_carrier(Msg)}]} || {carrier,MsgType,Msg} <- CarrierRecord].
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Cleans up our messages, ready for JSON/XML construction.
-%%
-%% We need this so that we can clean our date format, later we will
-%% format dates properly but this works as a quick fix.
-%% @spec clean_message(Carrier) -> XMLTuple
-%%
-%% @end
-%%--------------------------------------------------------------------
-clean_message(Data) when is_tuple(Data) ->
-    list_to_binary([httpd_util:rfc1123_date(Data)]);
-clean_message(Data) ->
-    list_to_binary(Data).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Loops over the record carrier building the tuple structure need to
-%% build out XML.
-%% @spec loop_xml_carrier(Carrier) -> XMLTuple
-%%
-%% @end
-%%--------------------------------------------------------------------
-loop_xml_carrier(CarrierRecord) ->
-    [loop_xml_tuple(DataType,clean_xml([Data])) || {carrier,DataType,Data} <- CarrierRecord].
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Loops over the inner record carriers building the tuple structure need to
-%% build out XML.
-%% @spec inner_loop_xml_carrier(CarrierRecord) -> [XMLTuple]
-%%
-%% @end
-%%--------------------------------------------------------------------
-inner_loop_xml_carrier(CarrierRecord) ->
-    [loop_xml_tuple(DataType,clean_xml([Data])) || {carrier,DataType,Data} <- CarrierRecord].
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Loops over the carrier tuple building the tuple structure need to
-%% build out XML.
-%% @spec inner_loop_xml_tuple(Messages) -> [XMLTuple]
-%%
-%% @end
-%%--------------------------------------------------------------------
-inner_loop_xml_tuple(Messages) ->
-    [loop_xml_tuple(Type,inner_loop_xml_carrier(Message))|| {carrier,Type,Message} <- Messages].
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Cleans our XML for us, especially modifying our date to a readable format.
-%% @spec clean_xml([Data]) -> XMLTuple
-%%
-%% @end
-%%--------------------------------------------------------------------
-clean_xml([Data]) when is_tuple(Data) ->
-    [httpd_util:rfc1123_date(Data)];
-clean_xml([Data]) ->
-    [Data].
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Prepares our tuple used to generate our XML.
-%% @spec xml_tuple(Type,Message) -> {XmlBody}
-%%
-%% @end
-%%--------------------------------------------------------------------
-xml_tuple(Type,Message) when is_list(Message) ->
-    case Message of
-	[Data] ->
-	    Data;
-	Data ->
-	    Data
-    end,
-    {chatterl,[],[{response,[],[{list_to_atom(Type),[],Data}]}]}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Prepares our tuple used to generate our sinlge XML elements.
-%% @spec xml_tuple_single(Type,Message) -> {XmlBody}
-%%
-%% @end
-%%--------------------------------------------------------------------
-xml_tuple_single(Type,Message) ->
-    {chatterl,[],[{response,[],[{list_to_atom(Type),[],[Message]}]}]}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Prepares our tuple used to generate our XML.
-%% @spec loop_xml_tuple(Type,Message) -> XmlTuple
-%%
-%% @end
-%%--------------------------------------------------------------------
-loop_xml_tuple(Type,Message) when is_list(Type) ->
-    {list_to_atom(Type),[],Message};
-loop_xml_tuple(Type,Message) ->
-    {list_to_atom(Type),[],[Message]}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Converts a tuple into XML.
-%%
-%% Inspired by the below link.
-%% <a target="_blank" href="http://arandomurl.com/post/Simple-XML-in-Erlang">Link</a>
-%% @spec tuple_to_xml(Xml,Prolog) -> [XML]
-%%
-%% @end
-%%--------------------------------------------------------------------
-tuple_to_xml(XmlTuple,Prolog) ->
-  lists:flatten(xmerl:export_simple([XmlTuple],xmerl_xml,[{prolog,Prolog}])).
 
 %%--------------------------------------------------------------------
 %% @private
