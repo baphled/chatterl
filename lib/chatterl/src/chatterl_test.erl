7bcdd4fc80438c587fcf7023c8c2313670cafb87
Yomi Colledge <baphled@Macintosh.gateway.2wire.net>
2009-03-03 23:23:46 +0000

    Added unit to test that a client can leave a group as expected.
    Have noticed that groups and clients aren't being dropped, have tried to do a setup but it fails for some reason, hardcoding chatterl_serv to stop also doesn't seem to help.

--- lib/chatterl/src/chatterl_test.erl
+++ lib/chatterl/src/chatterl_test.erl
@@ -18,16 +18,29 @@ chatterl_group_info_test_() ->
 
 %% Test that a client can connect to a group.
 chatterl_client_can_join_groups_test_() ->
+  Client = "noob",
+  Group = "anuva",
+  start_client(Client,Group,"anuva room"),
+  {timeout, 23,
+   [?_assertEqual([],gen_server:call({global,Group},list_users)),
+   ?_assertEqual({ok, "User added"},gen_server:call({global,Group},{join,Client})),
+   ?_assert(erlang:is_list(gen_server:call({global,Group},list_users))),
+   ?_assertEqual({error, "Already joined"},gen_server:call({global,Group},{join,"noob"})),
+   ?_assertEqual({error, "not connected"},gen_server:call({global,Group},{join,"nonUsers"})),
+   ?_assertEqual({error, "Invalid user name"},gen_server:call({global,Group},{join,{"nonUsers"}}))]}.
+
+start_client(Client,Group,Description) ->
   chatterl_serv:start(),
-  chatterl_groups:start("nu","a new room"),
-  chatterl_client:start("baft"),
-  gen_server:call({global,"nu"},{join,"baft"}),
-  [?_assert(erlang:is_list(gen_server:call({global,"nu"},list_users))),
-   ?_assertEqual({error, "Already joined"},gen_server:call({global,"nu"},{join,"baft"})),
-   ?_assertEqual({error, "not connected"},gen_server:call({global,"nu"},{join,"nonUsers"})),
-   ?_assertEqual({error, "Invalid user name"},gen_server:call({global,"nu"},{join,{"nonUsers"}}))].
+  chatterl_groups:start(Group,Description),
+  chatterl_client:start(Client).
+
+chatterl_client_can_leave_groups_test_() ->
+  Client = "baft",
+  Group = "nu",
+  start_client(Client,Group,"a nu room"),
+  [?_assertEqual({ok, "User added"},gen_server:call({global,Group},{join,"baft"}))].
 
 %% Test that our clients can connect to chatterl_serv & interact with Chatterl as we expect.
 chatterl_client_test_() ->
-  [?_assert({error,{already_started,<<"0.4711.0">>}} /= chatterl_client:start("bobby")),
+  [?_assert(erlang:is_tuple(chatterl_client:start("bobby"))),
   ?_assertMatch({error, "Unable to send message"},gen_server:call({global,"bobby"},{group_msg,"blah","hey"}))].
