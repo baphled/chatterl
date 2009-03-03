3c817a8e754b5533bb5a3626b4abc4b15cb09154
Yomi Colledge <baphled@Macintosh.gateway.2wire.net>
2009-03-03 22:36:08 +0000

    More test cases to cover a client joining a group.

--- lib/chatterl/src/chatterl_test.erl
+++ lib/chatterl/src/chatterl_test.erl
@@ -9,12 +9,29 @@ chatterl_serv_test_() ->
 
 %% Test that our groups function as expected.
 %% Instead of testing that the group can be created we'll create and group drops on setup.
-generate_group_tests_() ->
+chatterl_group_info_test_() ->
   chatterl_groups:start("nu","a new room"),
-  [?assertEqual("nu", gen_server:call({global,"nu"},name)),
-  ?assertEqual("a new room", gen_server:call({global,"nu"},description)),
-  ?assert(erlang:is_tuple(gen_server:call({global,"nu"},created)))].
+  [?_assertEqual({name,"nu"}, gen_server:call({global,"nu"},name)),
+   ?_assertEqual({description,"a new room"}, gen_server:call({global,"nu"},description)),
+   ?_assert(erlang:is_tuple(gen_server:call({global,"nu"},created))),
+   ?_assertEqual([], gen_server:call({global,"nu"},poll_messages))].
 
+%% Test that a client can connect to a group.
+chatterl_client_can_join_groups_test_() ->
+  chatterl_serv:start(),
+  chatterl_groups:start("nu","a new room"),
+  chatterl_client:start("baft"),
+  gen_server:call({global,"nu"},{join,"baft"}),
+  [?_assert(erlang:is_list(gen_server:call({global,"nu"},list_users)))].
+
+chatterl_client_error_on_duplicate_entry_test_() ->
+  chatterl_serv:start(),
+  chatterl_groups:start("nu","a new room"),
+  chatterl_client:start("baft"),
+  gen_server:call({global,"nu"},{join,"baft"}),
+  [?_assertEqual({error, "Already joined"},gen_server:call({global,"nu"},{join,"baft"})),
+   ?_assertEqual({error, "not connected"},gen_server:call({global,"nu"},{join,"nonUsers"})),
+    ?_assertEqual({error, "Invalid user name"},gen_server:call({global,"nu"},{join,{"nonUsers"}}))].
 %% Test that our clients can connect to chatterl_serv & interact with Chatterl as we expect.
 chatterl_client_test_() ->
   [?_assert({error,{already_started,<<"0.4711.0">>}} /= chatterl_client:start("bobby")),
