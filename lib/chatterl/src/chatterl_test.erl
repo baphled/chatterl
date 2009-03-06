2ef9163d7538ad4955ad3be248f293f34e58dc67
Yomi Colledge <baphled@Macintosh.(none)>
2009-03-06 20:14:13 +0000

    Refactored to use setup, will do further cleanup shortly.

--- lib/chatterl/src/chatterl_test.erl
+++ lib/chatterl/src/chatterl_test.erl
@@ -8,16 +8,15 @@ start_client(Client,Group,Description) ->
   chatterl_groups:start(Group,Description),
   chatterl_client:start(Client).
 
-%% Test Chatterl server.
-%%
-%% Todo Ideally we need to match the PID
-chatterl_serv_test_() ->
-  [?_assert({error,{already_started,<<"0.4711.0">>}} /= chatterl_serv:start())].
-
 %% Test that our groups function as expected.
 %% Instead of testing that the group can be created we'll create and group drops on setup.
 chatterl_group_info_test_() ->
-  chatterl_groups:start("test","a room"),
+  {setup,
+   fun() -> start_client("blah","test","a room") end,
+   fun(_Blah) -> chatterl_serv:stop() end,
+   fun test_group_info/0}.
+
+test_group_info() ->
   [?_assertEqual({name,"test"}, gen_server:call({global,"test"},name)),
    ?_assertEqual({description,"a room"}, gen_server:call({global,"test"},description)),
    ?_assert(erlang:is_tuple(gen_server:call({global,"test"},created))),
@@ -25,30 +24,46 @@ chatterl_group_info_test_() ->
 
 %% Test that a client can connect to a group.
 chatterl_client_can_join_groups_test_() ->
+  {setup,
+   fun() -> start_client("noob","anuva","a room") end,
+   fun(_) -> chatter_serv:stop() end,
+   fun test_client_can_join_groups/0}.
+
+test_client_can_join_groups() ->
   Client = "noob",
   Group = "anuva",
-  start_client(Client,Group,"anuva room"),
-  {timeout, 23,
    [?_assertEqual([],gen_server:call({global,Group},list_users)),
    ?_assertEqual({ok, "User added"},gen_server:call({global,Group},{join,Client})),
    ?_assert(erlang:is_list(gen_server:call({global,Group},list_users))),
    ?_assertEqual({error, "Already joined"},gen_server:call({global,Group},{join,"noob"})),
    ?_assertEqual({error, "not connected"},gen_server:call({global,Group},{join,"nonUsers"})),
-   ?_assertEqual({error, "Invalid user name"},gen_server:call({global,Group},{join,{"nonUsers"}}))]}.
+   ?_assertEqual({error, "Invalid user name"},gen_server:call({global,Group},{join,{"nonUsers"}}))].
 
 chatterl_client_can_leave_groups_test_() ->
+  {setup,
+   fun() -> start_client("noob","anuva","a room") end,
+   fun(_) -> chatter_serv:stop() end,
+   fun test_client_can_leave_groups/0}.
+
+generate_setup(Fun) ->
+  {setup,
+   fun() -> start_client("noob","anuva","a room") end,
+   fun(_) -> chatter_serv:stop() end,
+   Fun}.
+
+test_client_can_leave_groups() ->
   Client = "baft",
   Group = "nu",
-  start_client(Client,Group,"a nu room"),
   [?_assertEqual({ok, "User added"},gen_server:call({global,Group},{join,Client})),
    ?_assert(erlang:is_list(gen_server:call({global,Group},list_users))),
    ?_assertEqual({error, "Not connected"},gen_server:call({global,Group},{leave,"not a user"})),
    ?_assertEqual({ok, dropped},gen_server:call({global,Group},{leave,Client}))].
 
 chatterl_client_can_send_messages_test_() ->
-  Client = "noobie",
-  Group = "nu2",
-  start_client(Client,Group,"yet another room"),
+  generate_setup(fun client_can_send_messages/0).
+
+client_can_send_messages() ->
+  {Group,Client} = {"anuva","noob"},
   [?_assertEqual({error, user_not_joined},gen_server:call({global,Group},{send_msg,Client,"hey all"})),
    ?_assertEqual({ok, "User added"},gen_server:call({global,Group},{join,Client})),
    ?_assertEqual({ok, msg_sent},gen_server:call({global,Group},{send_msg,Client,"wassup"})),
