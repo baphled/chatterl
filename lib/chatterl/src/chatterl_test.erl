185411edbcc750a42c9637565d68ffea43ab8514
Yomi Colledge <baphled@Macintosh.(none)>
2009-03-07 19:26:05 +0000

    Added client tests.

--- lib/chatterl/src/chatterl_test.erl
+++ lib/chatterl/src/chatterl_test.erl
@@ -2,7 +2,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 start_client(Client,Group,Description) ->
-  {ok,Pid} = start_group(Group,Description),
+  {ok,Pid} = chatterl_serv:start(),
+  chatterl_groups:start(Group,Description),
   chatterl_client:start(Client),
   {ok,Pid}.
 
@@ -25,13 +26,31 @@ chatterl_group_info_test_() ->
   [{setup, fun() ->
                {Group,Description} = {"nu","a nu room"},
                {ok, Pid} = start_group(Group,Description),
-               register(chatterl_tests, Pid),
+               register(chatterl_groups_tests, Pid),
                Pid end,
-    fun(P) ->
-        exit(P, shutdown) end,
+    fun(_) ->
+        chatterl_serv:stop() end,
     [fun() ->
          Group = "nu",
          ?assertEqual({name,"nu"},gen_server:call({global,Group},name)),
          ?assertEqual({description,"a nu room"}, gen_server:call({global,Group},description)),
          ?assert(erlang:is_tuple(gen_server:call({global,Group},created))),
          ?assertEqual([],gen_server:call({global,Group},poll_messages)) end]}].
+
+%% Test that a client can connect to a group.
+chatterl_client_can_join_groups_test_() ->
+  [{setup, fun() ->
+               {Client,Group,Description} = {"noob","nu","a nu room"},
+               {ok, Pid} = start_client(Client,Group,Description),
+               register(chatterl_client_tests, Pid),
+               Pid end,
+    fun(P) ->
+        exit(P, shutdown) end,
+   [fun() ->
+        {Client,Group} = {"noob","nu"},
+        ?assertEqual([],gen_server:call({global,Group},list_users)),
+        ?assertEqual({ok, "User added"},gen_server:call({global,Group},{join,Client})),
+        ?assert(erlang:is_list(gen_server:call({global,Group},list_users))),
+        ?assertEqual({error, "Already joined"},gen_server:call({global,Group},{join,"noob"})),
+        ?assertEqual({error, "not connected"},gen_server:call({global,Group},{join,"nonUsers"})),
+        ?assertEqual({error, "Invalid user name"},gen_server:call({global,Group},{join,{"nonUsers"}})) end]}].
