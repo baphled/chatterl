e2c20b879b8538dac5a0d852eedae56cfbcdeaf4
Yomi Colledge <baphled@y-as-macbook.local>
2009-03-07 20:20:32 +0000

    Added units for sending messages.

--- lib/chatterl/src/chatterl_test.erl
+++ lib/chatterl/src/chatterl_test.erl
@@ -1,16 +1,6 @@
 -module(chatterl_test).
 -include_lib("eunit/include/eunit.hrl").
 
-start_client(Client,Group,Description) ->
-  {ok,Pid} = start_group(Group,Description),
-  chatterl_client:start(Client),
-  {ok,Pid}.
-
-start_group(Group,Description) ->
-  {ok, Pid} = chatterl_serv:start(),
-  chatterl_groups:start(Group,Description),
-  {ok,Pid}.
-
 %% Test that our groups function as expected.
 %% Instead of testing that the group can be created we'll create and group drops on setup.
 chatterl_serv_start_test_() ->
@@ -41,18 +31,36 @@ chatterl_client_can_join_groups_test_() ->
   [{setup, fun() ->
                {Client,Group,Description} = {"noob","nu","a nu room"},
                {ok, Pid} = start_client(Client,Group,Description),
-               register(chatterl_client_tests, Pid),
+               register(chatterl_client_join_tests, Pid),
                Pid end,
     fun(_) ->
         chatterl_serv:stop() end,
    [fun() ->
         {Client,Group} = {"noob","nu"},
         ?assertEqual([],gen_server:call({global,Group},list_users)),
+        ?assertEqual([],gen_server:call({global,Group},poll_messages)),
         ?assertEqual({ok, "User added"},gen_server:call({global,Group},{join,Client})),
         ?assert(erlang:is_list(gen_server:call({global,Group},list_users))),
         ?assertEqual({error, "Already joined"},gen_server:call({global,Group},{join,Client})),
         ?assertEqual({error, "not connected"},gen_server:call({global,Group},{join,"nonUsers"})),
         ?assertEqual({error, "Invalid user name"},gen_server:call({global,Group},{join,{"nonUsers"}})),
         ?assertEqual({ok, dropped}, gen_server:call({global,Group},{leave,Client})),
-        ?assertEqual({error, "Not connected"}, gen_server:call({global,Group},{leave,Client}))
+        ?assertEqual({error, "Not connected"}, gen_server:call({global,Group},{leave,Client})),
+        ?assertEqual([],gen_server:call({global,Group},list_users)),
+        ?assertEqual({ok, "User added"},gen_server:call({global,Group},{join,Client})),
+        ?assertMatch({error, user_not_joined},gen_server:call({global,Group},{send_msg,"blah","hey"})),
+        ?assertEqual({ok, msg_sent},gen_server:call({global,Group},{send_msg,Client,"hey"})),
+        ?assert(erlang:is_list(gen_server:call({global,Group},poll_messages)))
         end]}].
+
+
+%% Helper functions.
+start_client(Client,Group,Description) ->
+  {ok,Pid} = start_group(Group,Description),
+  chatterl_client:start(Client),
+  {ok,Pid}.
+
+start_group(Group,Description) ->
+  {ok, Pid} = chatterl_serv:start(),
+  chatterl_groups:start(Group,Description),
+  {ok,Pid}.
