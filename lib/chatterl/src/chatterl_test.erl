0bb15f4b2696f487d839bef7c12c2809f63e50fa
Yomi Colledge <baphled@boodah.net>
2009-03-27 10:30:12 +0000

    Added units for implementing register wrapper within chatterl_serv

--- lib/chatterl/src/chatterl_test.erl
+++ lib/chatterl/src/chatterl_test.erl
@@ -172,6 +172,26 @@ chatterl_mid_man_message_poll_test_() ->
           ?assert({struct,[{<<"messages">>,[]}]} /= mochijson2:decode(chatterl_mid_man:user_poll(["text/json"],Client2)))
       end}]}].
 
+chatterl_serv_register_test_() ->
+  {Nick1,Name1,Email1,Password1} = {"noobie","noobie 1","noobie@noobie.com","blahblah"},
+  {Nick2,Name2,Email2,Password2} = {"nerf","nerf 1","nerf@noobie.com","asfdasdf"},
+  [{setup,
+    fun() ->
+        chatterl:start(),
+        chatterl_store:start_link(ram_copies)
+    end,
+    fun(_) ->
+        chatterl:stop(),
+        chatterl_store:stop(),
+        mnesia:delete_table(registered_user)
+    end,
+    [{"Client can login via chatterl_serv",
+     fun() ->
+         ?assertEqual({error,"blah's passwords must match"},chatterl_serv:register("blah",{"yo","y@me.com","pass","pas"})),
+         ?assertEqual({ok,"noobie is registered"},chatterl_serv:register(Nick1,{Name1,Email1,Password1,Password1})),
+         ?assertEqual({error,"noobie is already registered"},chatterl_serv:register(Nick1,{Name1,Email1,Password1,Password1}))
+     end}]}].
+
 chatterl_mid_man_user_connect_test_() ->
   {Client1,Client2,Client3,Group} = {"baft","boodah","baphled","sum room"},
   [{setup,
